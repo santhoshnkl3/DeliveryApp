64fae212abdfddf96b4e5cbc2e3f0afcde5087f3
santhoshnkl3 <santhoshnkl3@gmail.com>
2019-10-02 15:49:27 +0530

    Updates

---
+++ precache-manifest.7239e32098fbfd57a633f3b8950e62f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e5eb244ba305adcf0a13e637f4959f",
+    "url": "/DeliveryApp/index.html"
+  },
+  {
+    "revision": "83529c3cbf07a3171132",
+    "url": "/DeliveryApp/static/css/main.1cb8be26.chunk.css"
+  },
+  {
+    "revision": "8df316eb544b48bf7155",
+    "url": "/DeliveryApp/static/js/2.e4aa6ffe.chunk.js"
+  },
+  {
+    "revision": "83529c3cbf07a3171132",
+    "url": "/DeliveryApp/static/js/main.dbb01669.chunk.js"
+  },
+  {
+    "revision": "89c4a575112be6fbda56",
+    "url": "/DeliveryApp/static/js/runtime-main.66f238c8.js"
+  }
+]);
